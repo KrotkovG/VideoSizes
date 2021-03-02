e7bbab6b85795a5bc4dbc56a20e32d654ae09c7e
Georgi Krotkov <krotkov.g@live.com>
2021-03-02 20:59:42 +0100

    zgolemeni kbps

--- src/com/company/HugeVideo.java
+++ src/com/company/HugeVideo.java
@@ -2,6 +2,6 @@ package com.company;
 
 public class HugeVideo {
     public static void main(String[] args) {
-        System.out.println("over 100GB ~ 125.000kbps");
+        System.out.println("over 100GB ~ 130.000kbps");
     }
 }
